29cbbc285dded9d57c29a750b76099e3bfabbab4
Ryan Armstrong <ryscar13@gmail.com>
2024-10-05 15:51:43 -0700

    make some files private

--- start_ollama.sh
+++ start_ollama.sh
@@ -3,6 +3,6 @@ docker kill ollama  # Stop any existing Docker container named 'ollama'.
 docker kill ollama
 docker rm ollama
 docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
-tmux new-session -d -s "ollama_chat" ./run_chat.sh
-tmux new-session -d -s "ollama_autocomplete" ./run_autocomplete.sh
-bash ./pull_embedding.sh
+tmux new-session -d -s "ollama_chat" ./_run_chat.sh
+tmux new-session -d -s "ollama_autocomplete" ./_run_autocomplete.sh
+bash ./_pull_embedding.sh
