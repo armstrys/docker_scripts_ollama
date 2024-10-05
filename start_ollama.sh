97c8be1f916e67ea59b4da1960db2f38311ddcdc
Ryan Armstrong <ryscar13@gmail.com>
2024-10-05 15:59:03 -0700

    remove duped line

--- start_ollama.sh
+++ start_ollama.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 docker kill ollama  # Stop any existing Docker container named 'ollama'.
-docker kill ollama
 docker rm ollama
 docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
 tmux new-session -d -s "ollama_chat" ./_run_chat.sh
